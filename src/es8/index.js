62bb9939deb9203bb475f0f87c854372bd76603c
Francisco Cervantes <francisco@soft.law>
2022-03-15 17:55:10 -0600

    es9

--- src/es8/index.js
+++ src/es8/index.js
@@ -33,3 +33,33 @@ console.log(string.padEnd(12,'---'));
 console.log('food'.padEnd(12,' ------'));
 
 
+//Async , Await
+
+//Promesa
+const helloWorld = () => {
+    return new Promise((resolve, reject)=>{
+        (false)
+            ? setTimeout(() => resolve("Hello world"), 3000)
+            : reject(new Error('Test Error'));
+    })
+};
+
+//Async
+const helloAsync = async () =>{
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () =>{
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch(error){
+        console.log(error)
+    }
+}
+
+anotherFunction();
+
